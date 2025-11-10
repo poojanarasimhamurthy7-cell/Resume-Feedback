29525f885790a3881cdf22cd75d92c253f0fcff3
poojanarasimhamurthy7-cell <poojanarasimhamurthy7@gmail.com>
2025-11-10 20:32:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function giveFeedback() {
+    const feedbackText = document.getElementById("feedbackText");
+
+    feedbackText.innerText = "✅ Your resume looks good! Add more technical skills, projects, and improve formatting for better clarity.";
+}
